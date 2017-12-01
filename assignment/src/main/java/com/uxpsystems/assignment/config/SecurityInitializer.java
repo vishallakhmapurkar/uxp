97516e1208e994ac30f2807299e66fcacf6f4c42
PK66DH <PK66DH@WPU8L0012370.ad.ing.net>
2017-12-01 15:18:56 +0100

    added security

---
+++ assignment/src/main/java/com/uxpsystems/assignment/config/SecurityInitializer.java
@@ -0,0 +1,5 @@
+package com.uxpsystems.assignment.config;
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+} 
---
+++ assignment/src/main/java/com/uxpsystems/assignment/config/WebAppInitializer.java
@@ -0,0 +1,17 @@
+package com.uxpsystems.assignment.config;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] { SecurityConfig.class };
+	}
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return null;
+	}
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+}
